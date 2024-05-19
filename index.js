b3ddc3f4c077af0100d1ef15eacb53e581920d23
Physical8 <leomaje@gmail.com>
2024-05-19 15:53:50 -0500

    Taller EJS data persons OK

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const debug = require('debug')('myapp:server');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const personsRouter = require('./routes/persons');
+app.use('/persons', personsRouter);
+
+app.listen(port, () => {
+    console.log(chalk.green(`Server is running on http://localhost:${port}`));
+    debug(`Server is running on http://localhost:${port}`);
+    console.log('Listening on port: ' + port);
+});
